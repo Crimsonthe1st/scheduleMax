8c20750ad8b4b92bc276d5169fccbebb3aeb9899
kenny <kennycs@protonmail.com>
2025-03-29 19:03:31 -0400

    Rendered practice dataframe

--- ui/Generator.py
+++ ui/Generator.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title='Generator'
@@ -12,8 +13,9 @@ st.divider()
 
 st.subheader('Preferences')
 
-classes = ['CSC 130', 'CSC 230', 'CSC 340', 'MAT 196', 'MAT 296', 'STA 290', 'CHE 351', 'CHE 352', 'BIO 330']
-previous_classes = st.multiselect('What previous classes have you passed?', classes)
+# TODO: Pre-reqs
+courses = [('CSC 130', 3), ('CSC 230', 3), ('CSC 340', 3), ('MAT 196', 4), ('MAT 296', 3), ('STA 290', 3), ('CHE 351', 3), ('CHE 352', 3), ('BIO 330', 3)]
+previous_courses = st.multiselect('What previous courses have you passed?', courses)
 
 credits = st.slider('What are your total credit hours?', 0, 150)
 
@@ -26,3 +28,20 @@ min_hours, max_hours = st.select_slider(
 majors = ['Computer Science', 'Mathematics', 'Biology']
 current_majors = st.multiselect('What are your majors?', majors)
 current_minors = st.multiselect('What are your minors?', majors)
+
+fall2025_courses = [0, 3, 5, 6]
+example_schedule_fall2025 = pd.DataFrame(
+    {
+        'courses': [courses[i][0] for i in fall2025_courses],
+        'hours': [courses[i][1] for i in fall2025_courses],        
+    }
+)
+
+st.dataframe(
+    example_schedule_fall2025,
+    column_config = {
+        'courses': 'Courses',
+        'hours': 'Credit Hours',
+    },
+    hide_index=True,
+)
